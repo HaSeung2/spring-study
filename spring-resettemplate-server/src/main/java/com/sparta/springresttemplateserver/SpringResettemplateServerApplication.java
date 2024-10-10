7fe563600cd1114df81b4885fbbf75cce6061224
하승 <dlgktmd0@naver.com>
2024-10-10 11:50:43 +0900

    init commit

---
+++ spring-resettemplate-server/src/main/java/com/sparta/springresttemplateserver/SpringResettemplateServerApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.springresttemplateserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringResettemplateServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringResettemplateServerApplication.class, args);
+    }
+
+}
